303bc5852abcf5690fe552ab1cf110510d75ea46
ledavto <ser_ega@ukr.net>
2023-08-23 22:32:41 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені!");
+  }
+    const formValue = {};
+    formValue.email = email;
+    formValue.password = password;
+  console.log(formValue);
+  form.reset();
+}
