ae9dddd7603d17c2a5642d430fbae4454c38a654
ledavto <ser_ega@ukr.net>
2023-08-24 22:57:45 +0300

    edit HW

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,7 @@ const item = document.querySelectorAll(".item");
 console.log(`Number of categories: ${item.length}`);
 console.log('');
 
-var elems = item;
-elems = Array.prototype.slice.call(elems);
-
-elems.forEach(element => {
-   // if (element === 'ul#categories')
+item.forEach(element => {
     console.log('Category: ', element.firstElementChild.textContent);
     console.log('Elements: ', element.lastElementChild.children.length);
     console.log('');
--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,13 @@ const ingredients = [
   'Condiments',
 ];
 
-const arrayLi = [];
-
-ingredients.forEach((elem, index) => {
-  arrayLi[index] = document.createElement('li');
-  arrayLi[index].textContent = elem;
-  arrayLi[index].classList.add('item');
+const arrayLi = ingredients.map((elem, index) => {
+  const temp = document.createElement('li');
+ 
+  elem.textContent = elem;
+  elem.classList.add('item');
 });
 
 const findUl = document.querySelector("ul");
-findUl.append(arrayLi[0], arrayLi[1], arrayLi[2], arrayLi[3], arrayLi[4],arrayLi[5]);
+findUl.append(...arrayLi);
 
