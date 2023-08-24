aadee6c299bd0ca4b9931d76fd513e27d42e953b
ledavto <ser_ega@ukr.net>
2023-08-24 10:42:13 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+console.log(btnEl);
+
+btnEl.addEventListener('click', () => {
+  const randColor = getRandomHexColor();
+  spanEl.textContent = randColor;
+  document.body.style.backgroundColor = randColor;
+});
+
